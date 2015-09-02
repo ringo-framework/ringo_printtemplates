d8cedfe42784e6cfe9f4d4e5cb8fe58aa60845d8
Torsten Irländer <torsten.irlaender@intevation.de>
2015-09-02 19:47:43 +0200

    Fixed renderer. Fixed names of the extension and changes some things which have changed in the lates ringo version.

--- ringo_printtemplate/lib/renderer.py
+++ ringo_printtemplate/lib/renderer.py
@@ -4,18 +4,22 @@ from mako.lookup import TemplateLookup
 from formbar.config import Config, load
 from formbar.form import Form
 
+import ringo.lib.helpers
 from ringo.lib.helpers import (
-    get_item_modul
+    get_item_modul,
+    get_app_url,
+    literal
 )
 from ringo.lib.form import (
-    eval_url,
+    get_eval_url,
     get_path_to_form_config,
 )
 from ringo.lib.renderer.dialogs import DialogRenderer
 
-base_dir = pkg_resources.get_distribution("ringo_printtemplates").location
-template_dir = os.path.join(base_dir, 'ringo_printtemplates', 'templates')
-template_lookup = TemplateLookup(directories=[template_dir])
+base_dir = pkg_resources.get_distribution("ringo_printtemplate").location
+template_dir = os.path.join(base_dir, 'ringo_printtemplate', 'templates')
+template_lookup = TemplateLookup(directories=[template_dir],
+                                 default_filters=['h'])
 
 
 class PrintDialogRenderer(DialogRenderer):
@@ -24,16 +28,18 @@ class PrintDialogRenderer(DialogRenderer):
     def __init__(self, request, clazz):
         """@todo: to be defined """
         DialogRenderer.__init__(self, request, clazz, "print")
-        self.template = template_lookup.get_template("print.mako")
+        self.template = template_lookup.get_template("internal/print.mako")
         config = Config(load(get_path_to_form_config('print.xml',
-                                                     'ringo_printtemplates')))
+                                                     'ringo_printtemplate',
+                                                     '.')))
         form_config = config.get_form('default')
         self.form = Form(form_config,
                          item=clazz,
                          csrf_token=self._request.session.get_csrf_token(),
                          dbsession=request.db,
-                         eval_url=eval_url,
-                         url_prefix=request.application_url)
+                         translate=request.translate,
+                         url_prefix=get_app_url(request),
+                         eval_url=get_eval_url())
 
     def render(self):
         values = {}
@@ -45,10 +51,11 @@ class PrintDialogRenderer(DialogRenderer):
         values['ok_url'] = self._request.current_route_path()
         values['_'] = self._request.translate
         values['cancel_url'] = self._request.referrer
-        values['eval_url'] = self._request.application_url + eval_url
-        return self.template.render(**values)
+        values['eval_url'] = self.form._eval_url
+        values['h'] = ringo.lib.helpers
+        return literal(self.template.render(**values))
 
     def _render_body(self):
         out = []
         out.append(self.form.render(buttons=False))
-        return "".join(out)
+        return literal("").join(out)
